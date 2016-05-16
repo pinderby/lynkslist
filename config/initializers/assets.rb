9c0490252c4d917b50b6ff8e61ceea69da0acd4b
David Mangold <pinderby@gmail.com>
2016-05-15 19:13:43 -0700

    Added glyphicons to precompiled assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += [
-	'main.css'
+	'main.css',
+	'twitter/bootstrap/glyphicons-halflings.png',
+	'twitter/bootstrap/glyphicons-halflings-white.png'
 ]
 
 # Add additional assets to the asset load path
